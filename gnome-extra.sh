efac6d5928e97207cad72fea4cdc5179006723ca
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-03 10:10:58 +0530

    fix: syntax issue with apt firefox rule

--- gnome-extra.sh
+++ gnome-extra.sh
@@ -10,11 +10,11 @@ install_firefox() {
     apt update && apt install firefox software-properties-common -y
     add-apt-repository --yes ppa:mozillateam/ppa
     
-    echo '
-    Package: *
-    Pin: release o=LP-PPA-mozillateam
-    Pin-Priority: 1001
-    ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
     echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
     apt remove firefox -y
