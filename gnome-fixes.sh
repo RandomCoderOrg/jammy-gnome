c97b2786ee000bf0cfcf92e0fbe44b90fe04feae
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-08 14:11:32 +0530

    add new fix for gnome-control-center

--- gnome-fixes.sh
+++ gnome-fixes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# disable org.freedesktop.login1 for gdm
+# 001 disable org.freedesktop.login1 for gdm
 # issue: login1 depends on systemd which wont work in proot & chroot
 #        gnome-shell triggers login1 and fails to start cause it returns exit code 1
 # solution: removing login1 files from entire filesystem or moving it to a different location should work
@@ -10,16 +10,20 @@ for file in $(find /usr -type f -iname "*login1*"); do
     mv -v $file "$file.back"
 done
 
-# fix issue with set-uid helper permission
+# 002 fix issue with set-uid helper permission
 # issue: gnome-shell fails to start cause it can't execute set-uid helper
 # solution: set permissions u+s for set-uid helper on every bashrc execution
 
 echo "# fix issue with set-uid helper permission" >> ~/.bashrc
 echo "sudo chmod u+s /usr/lib/dbus-1.0/dbus-daemon-launch-helper" >> ~/.bashrc
 
-# Remove sound menu from gnome control center
+# 003 Remove sound menu from gnome control center
 # issue: gnome-control-center crashes when sound menu is clicked
 # solution: remove sound menu from gnome-control-center by moving it to a different location
 
 mv -v /usr/share/applications/gnome-sound-panel.desktop /usr/share/applications/gnome-sound-panel.desktop.back
 
+# 004 set XDG_CURRENT_DESKTOP to GNOME
+# resolvs issue with gnome-control-center launch
+# observed on ubuntu lunar
+echo "export XDG_CURRENT_DESKTOP=GNOME" >> ~/.bashrc
